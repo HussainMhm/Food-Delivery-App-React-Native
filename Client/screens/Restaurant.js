fe782ae2a34ad798c124c4989c924dcf94171dc4
hussain <hussainabdullatif@marun.edu.tr>
2023-11-20 21:10:24 +0300

    Finished up restaurant details and menu screen

---
+++ Client/screens/Restaurant.js
@@ -0,0 +1,82 @@
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+import * as Icon from "react-native-feather";
+import { themeColors } from "../theme";
+
+import DishRow from "../components/DishRow";
+import CartIcon from "../components/CartIcon";
+import { StatusBar } from "expo-status-bar";
+
+function Restaurant({ route, navigation }) {
+    const item = route.params;
+
+    return (
+        <View>
+            <CartIcon />
+            <StatusBar style="light" />
+            <ScrollView>
+                {/* Header Banner */}
+                <View className="relative">
+                    <Image className="w-full h-72" source={item.image} />
+                    <TouchableOpacity
+                        className="absolute top-14 left-4 bg-gray-50 p-2 rounded-full shadow"
+                        onPress={() => navigation.goBack()}
+                    >
+                        <Icon.ArrowLeft stroke={themeColors.bgColor(1)} strokeWidth={3} />
+                    </TouchableOpacity>
+                </View>
+
+                {/* Content */}
+                <View
+                    style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+                    className="bg-white -mt-12 pt-6"
+                >
+                    <View className="px-5">
+                        {/* Restaurant Name */}
+                        <Text className="text-3xl font-bold">{item.name}</Text>
+
+                        {/* Restaurant Details */}
+                        <View className="flex-row space-x-2 my-2">
+                            <View className="flex-row items-center space-x-1">
+                                <Image
+                                    source={require("../assets/images/fullStar.png")}
+                                    className="h-4 w-4"
+                                />
+                                <Text className="text-sm">
+                                    <Text className="text-green-700">{item.rating}</Text>
+                                    <Text className="text-gray-700">
+                                        {" "}
+                                        ({item.reviews} review)
+                                    </Text>{" "}
+                                    ·{" "}
+                                    <Text className="font-semibold text-gray-700">
+                                        {item.category}
+                                    </Text>
+                                </Text>
+                            </View>
+
+                            <View className="flex-row items-center space-x-1 ">
+                                <Icon.MapPin height={15} width={15} stroke="gray" />
+                                <Text className="text-gray-700 text-xs">{item.address}</Text>
+                            </View>
+                        </View>
+
+                        {/* Restaurant Description */}
+                        <Text className="text-gray-500 mt-2">{item.description}</Text>
+                    </View>
+                </View>
+
+                {/* Menu */}
+                <View className="pb-36 bg-white">
+                    <Text className="px-4 py-4 text-2xl font-bold">Menu</Text>
+
+                    {/* Dishes */}
+                    {item.dishes.map((dish, index) => {
+                        return <DishRow item={{ ...dish }} key={index} />;
+                    })}
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
+
+export default Restaurant;
