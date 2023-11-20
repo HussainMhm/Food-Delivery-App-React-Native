f3060beba1c70e722075d0b3eff867f488019624
hussain <hussainabdullatif@marun.edu.tr>
2023-11-20 20:50:11 +0300

    Set the navigation configurations

--- Client/App.js
+++ Client/App.js
@@ -1,11 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./navigation";
 
 export default function App() {
-    return (
-        <View className="flex-1 justify-center items-center">
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+    return <Navigation />;
 }
---
+++ Client/navigation.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./screens/Home";
+import Restaurant from "./screens/Restaurant";
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Restaurant" component={Restaurant} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
