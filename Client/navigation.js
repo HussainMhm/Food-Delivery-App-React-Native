399d452856b9b74eeea0d34e8d35cdd1a4c657ed
hussain <hussainabdullatif@marun.edu.tr>
2023-11-20 22:02:38 +0300

    Update navigation.js

--- Client/navigation.js
+++ Client/navigation.js
@@ -3,6 +3,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./screens/Home";
 import Restaurant from "./screens/Restaurant";
+import Cart from "./screens/Cart";
+import OrderPreparing from "./screens/OrderPreparing";
+import Delivery from "./screens/Delivery";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +15,17 @@ export default function Navigation() {
             <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Restaurant" component={Restaurant} />
+                <Stack.Screen name="Cart" component={Cart} options={{ presentation: "modal" }} />
+                <Stack.Screen
+                    name="OrderPreparing"
+                    component={OrderPreparing}
+                    options={{ presentation: "fullScreenModal" }}
+                />
+                <Stack.Screen
+                    name="Delivery"
+                    component={Delivery}
+                    options={{ presentation: "fullScreenModal" }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
